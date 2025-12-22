1be9df8d1dbee732ac243bfd7ad001327fdd077f
JohnNPrice <paolo.dalsasso2@gmail.com>
2025-12-22 18:44:41 +0100

    mongo_resotre_lf

--- mongo-restore.sh
+++ mongo-restore.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Restoring MongoDB dump..."
+echo "Restoring MongoDB dump (dropping existing data)..."
 
 mongorestore \
+  --drop \
   --username "$MONGO_INITDB_ROOT_USERNAME" \
   --password "$MONGO_INITDB_ROOT_PASSWORD" \
   --authenticationDatabase admin \
